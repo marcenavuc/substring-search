a7b0ed9a5f2a6ceeff2b7771b440058182f0b869
marcenavuc <markenavuk@bk.ru>
2020-04-23 11:42:02 +0500

    Added support pandas. New feature: save results in results.xlsx, save some statistic in stat.xlsx. Removed support pickle

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,8 @@
-import time
-import pickle
 from tests.algorithm_test import ALGORITHMS, TestAlgorithms
 import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
-
-
-def delta_time(func, *args, **kwargs):
-    start = time.time()
-    func(*args, **kwargs)
-    end = time.time()
-    return end - start
+import timeit
 
 
 class Benchmark:
@@ -29,29 +22,33 @@ class Benchmark:
             for test in self.tests:
                 for i in range(self.n_times):
                     self.test_time[test.name][algorithm.name].append(
-                        delta_time(algorithm.search, test.substring, test.text)
-                    )
-        self.save_results()
+                        timeit.timeit('algorithm.search(test.substring, '
+                                      'test.text)',
+                                      globals=locals(),
+                                      number=1))
 
-    def save_results(self):
-        with open('results.pickle', 'wb') as file:
-            pickle.dump(self.test_time, file)
+    def save_results(self, table_name, func=lambda table: table):
+        writer = pd.ExcelWriter(table_name)
+        for test in self.tests:
+            test_table = pd.DataFrame(self.test_time[test.name],
+                                      columns=[alg.name for alg in ALGORITHMS])
+            func(test_table).to_excel(writer, sheet_name=test.name)
+
+        writer.save()
 
     def report(self):
-        with open('results.pickle', 'rb') as file:
-            data = pickle.load(file)
-
-        exp = 1e7
+        data = self.test_time
+        exp = 1e5
         figure, axises = plt.subplots(len(data), figsize=(15, 30))
         for i, test in enumerate(data):
             for alg in data[test]:
                 x_arr = range(len(data[test][alg]))
                 y_arr = list(map(lambda x: x * exp, data[test][alg]))
 
-                axises[i].scatter(x_arr, y_arr,label=alg)
+                axises[i].scatter(x_arr, y_arr, label=alg)
                 axises[i].set_title(test)
                 axises[i].set_xlabel('runs')
-                axises[i].set_ylabel(f'time powed {exp}')
+                axises[i].set_ylabel('time powed %s' % exp)
                 axises[i].legend()
         figure.savefig('report.png', format='png')
 
@@ -63,11 +60,23 @@ if __name__ == '__main__':
     parser.add_argument('N', metavar='N_times', type=int, help='an integer of '
                                                                'times to run '
                                                                'tests')
-    parser.add_argument('--report', dest='make_report', action='store_const',
-                        const=True, default=False, help='makes report of tests'
-                                                       'in report.png')
+    parser.add_argument('--report-img', '-r', dest='is_report',
+                        action='store_true',
+                        help='makes report of tests in report.png')
+    parser.add_argument('--save-results', '-s', dest='is_save',
+                        action='store_true',
+                        help='save results in results.xlsx')
+    parser.add_argument('--stat', dest='is_stat',
+                        action='store_true',
+                        help='save some statistic in stat.xlsx')
+
     args = parser.parse_args()
     benchmark = Benchmark(args.N)
     benchmark.run()
-    if args.make_report:
+
+    if args.is_save:
+        benchmark.save_results('results.xlsx')
+    if args.is_stat:
+        benchmark.save_results('stat.xlsx', func=lambda table: table.describe())
+    if args.is_report:
         benchmark.report()
